5994e2368c0e6b50610940ebf97f44175551d631
mithun85kol-png <mithun85kol@gmail.com>
2026-07-26 22:36:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,4 @@
-import os
-import requests
-from datetime import datetime
+print("EMA Alert Bot Started")
 
-BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
-
-def send_message(msg):
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    requests.post(url, data={
-        "chat_id": CHAT_ID,
-        "text": msg
-    })
-
-if __name__ == "__main__":
-    send_message(
-        f"""🚀 EMA ALERT BOT STARTED
-
-✅ GitHub Action Running
-🕒 {datetime.now().strftime('%d-%m-%Y %H:%M:%S')}
-"""
-    )
+while True:
+    print("Checking NIFTY 50...")
